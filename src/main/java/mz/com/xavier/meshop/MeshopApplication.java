1f1922d89e6da9c98d6378dfc4e9f8f8805223bc
Xavier Nhagumbe <xavierfrancisco353@gmail.com>
2019-04-06 11:21:42 +0200

    Initialize...

---
+++ src/main/java/mz/com/xavier/meshop/MeshopApplication.java
@@ -0,0 +1,13 @@
+package mz.com.xavier.meshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeshopApplication.class, args);
+	}
+
+}
