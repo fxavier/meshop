edfcab58355381631d9f958df283e6ce571f0d9a
Xavier Nhagumbe <Xavier_Nhagumbe@pmivectorlink.com>
2019-04-13 12:45:01 +0200

    Security initilized

--- src/main/java/mz/com/xavier/meshop/resource/SubcategoriaResource.java
+++ src/main/java/mz/com/xavier/meshop/resource/SubcategoriaResource.java
@@ -7,11 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.List;
 
-@Resource
+@RestController
 @RequestMapping("/subcategorias")
 public class SubcategoriaResource {
 
